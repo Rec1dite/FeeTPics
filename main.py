b699a948299a24efc1dccf21b9fba295fda92a7a
Rec1dite <rec1dite@gmail.com>
2023-05-21 01:10:32 +0200

    Fixed broken git merge :P

--- main.py
+++ main.py
@@ -1,30 +1,28 @@
 # pylint: disable=missing-module-docstring, wildcard-import, unused-wildcard-import, missing-function-docstring, invalid-name, missing-class-docstring
 import time
 from ftp import sendFile
-from ftp import deleteFile
 from watchdog.events import FileSystemEventHandler
-from watchdog.events import FileSystemEvent
-from watchdog.events import FileSystemMovedEvent
 from watchdog.observers import Observer
 from configs import *
 
 
 class UpdateEventHandler(FileSystemEventHandler):
-    def on_created(self, event:FileSystemEvent):
-        sendFile(event.src_path)
+    def on_created(self, event):
+        connectFPT()
 
-    def on_moved(self, event:FileSystemMovedEvent):
-        # This is very bad code you are supposed to use the Rename From(RNFR) followed by Rename to (RNTO) command.
-        sendFile(event.dest_path)
-        deleteFile(event.src_path)
+    def on_moved(self, event):
+        connectFPT()
 
-    def on_deleted(self, event:FileSystemEvent):
-        deleteFile(event.src_path)
+    def on_deleted(self, event):
+        connectFPT()
 
-    def on_modified(self, event:FileSystemEvent):
-        sendFile(event.src_path)
+    def on_modified(self, event):
+        connectFPT()
 
 if __name__ == '__main__':
+
+    sendFile("./test.txt")
+    exit()
 
     observer = Observer()
     eventHandler = UpdateEventHandler()
